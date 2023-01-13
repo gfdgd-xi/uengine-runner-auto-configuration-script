f745d3ebfcdd2c1299336aae43e06e81059515ec
gfdgd_xi <3025613752@qq.com>
2023-01-13 22:30:08 +0800

    新增程序 安装 一起作业老师 2.8.4.1000

---
+++ 17teacher_2.8.4.1000_302002.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env uengine-runner-auto-install-bash
+# 使用 UEngine 运行器（原 Wine 运行器）的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月13日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/17teacher_2.8.4.1000_302002.apk"
+fi
+rm "/tmp/17teacher_2.8.4.1000_302002.apk"
+download "https://cdn-app.17zuoye.cn/resources/app/17teacher/2.8.4.1000/17teacher_2.8.4.1000_302002.apk" /tmp "17teacher_2.8.4.1000_302002.apk"
+installapk "/tmp/17teacher_2.8.4.1000_302002.apk"
+info 提示 安装完成！
+rm -rfv "/tmp/17teacher_2.8.4.1000_302002.apk"
