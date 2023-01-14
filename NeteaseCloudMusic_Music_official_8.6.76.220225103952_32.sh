9804414feeff3cfa9f412bca158934b6645e8303
gfdgd_xi <3025613752@qq.com>
2023-01-14 09:54:29 +0800

    新增程序 安装 网易云音乐 Android 5.X 8.6.76.22

---
+++ NeteaseCloudMusic_Music_official_8.6.76.220225103952_32.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env uengine-runner-auto-install-bash
+# 使用 UEngine 运行器（原 Wine 运行器）的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月14日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/NeteaseCloudMusic_Music_official_8.6.76.220225103952_32.apk"
+fi
+rm "/tmp/NeteaseCloudMusic_Music_official_8.6.76.220225103952_32.apk"
+download "https://d1.music.126.net/dmusic/NeteaseCloudMusic_Music_official_8.6.76.220225103952_32.apk" /tmp "NeteaseCloudMusic_Music_official_8.6.76.220225103952_32.apk"
+installapk "/tmp/NeteaseCloudMusic_Music_official_8.6.76.220225103952_32.apk"
+info 提示 安装完成！
+rm -rfv "/tmp/NeteaseCloudMusic_Music_official_8.6.76.220225103952_32.apk"
