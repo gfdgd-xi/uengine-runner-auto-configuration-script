e4063c40bf940242c3740a9a046ac0a3eb1cdef4
gfdgd_xi <3025613752@qq.com>
2023-01-14 09:55:42 +0800

    新增程序 安装 QQ音乐 12.0.5.8

---
+++ QQMusic2005000982.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env uengine-runner-auto-install-bash
+# 使用 UEngine 运行器（原 Wine 运行器）的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月14日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/QQMusic2005000982.apk"
+fi
+rm "/tmp/QQMusic2005000982.apk"
+download "http://dldir1v6.qq.com/music/clntupate/QQMusic2005000982.apk" /tmp "QQMusic2005000982.apk"
+installapk "/tmp/QQMusic2005000982.apk"
+info 提示 安装完成！
+rm -rfv "/tmp/QQMusic2005000982.apk"
