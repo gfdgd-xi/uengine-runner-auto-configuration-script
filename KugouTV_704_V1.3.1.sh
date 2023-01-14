0e2356d6126198b0abf3bdf91c76c72b8ef4b33e
gfdgd_xi <3025613752@qq.com>
2023-01-14 09:58:43 +0800

    新增程序 安装 酷狗音乐TV版 1.3.1

---
+++ KugouTV_704_V1.3.1.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env uengine-runner-auto-install-bash
+# 使用 UEngine 运行器（原 Wine 运行器）的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月14日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/KugouTV_704_V1.3.1.apk"
+fi
+rm "/tmp/KugouTV_704_V1.3.1.apk"
+download "https://packagebssdlbig.kugou.com/upload/android/KugouTV_704_V1.3.1.apk" /tmp "KugouTV_704_V1.3.1.apk"
+installapk "/tmp/KugouTV_704_V1.3.1.apk"
+info 提示 安装完成！
+rm -rfv "/tmp/KugouTV_704_V1.3.1.apk"
