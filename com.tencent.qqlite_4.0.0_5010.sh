e757bbd0d718dc66144753e8a1ad8350393795a4
gfdgd_xi <3025613752@qq.com>
2023-01-17 19:15:15 +0800

    新增程序 安装 QQ极速版4.0.0

---
+++ com.tencent.qqlite_4.0.0_5010.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env uengine-runner-auto-install-bash
+# 使用 UEngine 运行器（原 Wine 运行器）的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月17日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/com.tencent.qqlite_4.0.0_5010.apk"
+fi
+rm "/tmp/com.tencent.qqlite_4.0.0_5010.apk"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library-android/raw/branch/master/com.tencent.qqlite_4.0.0_5010.apk?gfdgd_xi=gfdgd_xi" /tmp "com.tencent.qqlite_4.0.0_5010.apk"
+installapk "/tmp/com.tencent.qqlite_4.0.0_5010.apk"
+info 提示 安装完成！
+rm -rfv "/tmp/com.tencent.qqlite_4.0.0_5010.apk"
