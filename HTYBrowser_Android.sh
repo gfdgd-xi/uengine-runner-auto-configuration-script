4f86a393d8ae609076b628bc706b16c6b75676ab
gfdgd_xi <3025613752@qq.com>
2023-01-20 21:59:17 +0800

    新增程序 安装 海天鹰浏览器

---
+++ HTYBrowser_Android.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env uengine-runner-auto-install-bash
+# 使用 UEngine 运行器（原 Wine 运行器）的语言解析器
+##########################################################################################
+# 作者：gfdgd xi、为什么您不喜欢熊出没和阿布呢
+# 更新时间：2023年1月20日
+##########################################################################################
+# 用于判断是否为 bash 解释器
+if [[ 1 == 2 ]]; then
+    # 保持对旧版本的兼容
+    bash rm "/tmp/HTYBrowser_Android.apk"
+fi
+rm "/tmp/HTYBrowser_Android.apk"
+download "https://code.gitlink.org.cn/gfdgd_xi/program-library-android/raw/branch/master/HTYBrowser_Android.apk?gfdgd_xi=gfdgd_xi" /tmp "HTYBrowser_Android.apk"
+installapk "/tmp/HTYBrowser_Android.apk"
+info 提示 安装完成！
+rm -rfv "/tmp/HTYBrowser_Android.apk"
